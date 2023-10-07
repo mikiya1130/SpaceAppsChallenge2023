0ced0f52fb114b789e186bc185eb006e69f4bc90
udonmogumogu <cocoa.m.1024@gmail.com>
2023-10-07 20:25:00 +0900

    update: generateLineRenderer完成（ピンクに沿って移動しない）

--- Assets/LineMover.cs
+++ Assets/LineMover.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class LineMover : MonoBehaviour
 {
-    [SerializeField] private LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
     [SerializeField] private float speed = 1f;
 
     private int currentIndex;
     public EventHandler OnEndReached;
+    private Vector3 previous_position;
 
     private void Start()
     {
+        int[] values = {1, 4, 3, 5, 2, 6};
+        lineRenderer = generateLineRenderer(values);
         Initialize(0,speed,lineRenderer);
-        Debug.Log("aaa");
-
+        previous_position = transform.position;
     }
     public void Initialize(int index, float speed, LineRenderer lineRenderer)
     {
@@ -52,13 +56,37 @@ public class LineMover : MonoBehaviour
         var result = GetTargetPosition(
             ref currentIndex,
             speed * Time.deltaTime,
-            transform.position,
+            previous_position,
             lineRenderer);
-        transform.position = result.targetPosition;
+            
+        Vector3 shift = new Vector3(0.0f, 1.0f, 0.0f); 
+        previous_position = result.targetPosition;
+        transform.position = result.targetPosition + shift;
         if (result.isEnd)
         {
             OnEndReached?.Invoke(this, EventArgs.Empty);
             enabled = false;
         }
+    }
+
+    private LineRenderer generateLineRenderer(int[] values){
+
+        // LineRendererコンポーネントをゲームオブジェクトにアタッチする
+        var lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        //線の幅を設定
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+
+        List<int> indexes = Enumerable.Range(0, values.Length).ToList();
+        Vector3[] positions = indexes.Select(i => new Vector3(i, values[i], 0.0f)).ToArray();
+
+        lineRenderer.positionCount = positions.Length;
+
+        // 線を引く場所を指定する
+        lineRenderer.SetPositions(positions);
+
+        return lineRenderer;
+
     }
 }
