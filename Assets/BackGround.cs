1f2b4b6a10949d2757b6ec797003e0397914fd9f
gunsow <fukudatatsuya911@gmail.com>
2023-10-08 15:36:40 +0900

    背景の色を時間で変化するようにした

--- Assets/BackGround.cs
+++ Assets/BackGround.cs
@@ -1,42 +1,19 @@
 using UnityEngine;
-using UnityEngine.UI;
 
-public class BackGroundMover : MonoBehaviour
+public class BackGround : MonoBehaviour
 {
-	private const float k_maxLength = 1f;
-	private const string k_propName = "_MainTex";
-
 	[SerializeField]
-	private Vector2 m_offsetSpeed;
+	private Gradient gradient = default;
+	private float currentTime = 0.0f;
 
-	private Material m_material;
-	
 	private void Start()
 	{
-		if (GetComponent<Image>() is Image i)
-		{
-			m_material = i.material;
-		}
 	}
 
 	private void Update()
 	{
-		if (m_material)
-		{
-			// xとyの値が0 ～ 1でリピートするようにする
-			var x = Mathf.Repeat(Time.time * m_offsetSpeed.x, k_maxLength);
-			var y = Mathf.Repeat(Time.time * m_offsetSpeed.y, k_maxLength);
-			var offset = new Vector2(x, y);
-			m_material.SetTextureOffset(k_propName, offset);
-		}
-	}
-
-	private void OnDestroy()
-	{
-		// ゲームをやめた後にマテリアルのOffsetを戻しておく
-		if (m_material)
-		{
-			m_material.SetTextureOffset(k_propName, Vector2.zero);
-		}
+		currentTime += Time.deltaTime * 0.005f;
+		if (currentTime > 1.0f) currentTime = 0.0f;
+		Camera.main.backgroundColor = gradient.Evaluate(currentTime);
 	}
 }
